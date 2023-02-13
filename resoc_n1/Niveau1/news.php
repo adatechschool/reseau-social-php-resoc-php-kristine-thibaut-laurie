2ac29419a24943decc34081aeb80c71d0cd38a47
LaurieBiguet <biguet.laurie@live.fr>
2023-02-13 14:35:27 +0100

    V1

--- resoc_n1/Niveau1/news.php
+++ resoc_n1/Niveau1/news.php
@@ -66,7 +66,7 @@
                  */
 
                 // Etape 1: Ouvrir une connexion avec la base de donnée.
-                $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+                include './config.php';
                 //verification
                 if ($mysqli->connect_errno)
                 {
@@ -111,7 +111,7 @@
                 {
                     //la ligne ci-dessous doit etre supprimée mais regardez ce 
                     //qu'elle affiche avant pour comprendre comment sont organisées les information dans votre 
-                    echo "<pre>" . print_r($post, 1) . "</pre>";
+                    //echo "<pre>" . print_r($post, 1) . "</pre>";
 
                     // @todo : Votre mission c'est de remplacer les AREMPLACER par les bonnes valeurs
                     // ci-dessous par les bonnes valeurs cachées dans la variable $post 
@@ -125,7 +125,7 @@
                         </h3>
                         <address>AREMPLACER</address>
                         <div>
-                            <p>AREMPLACER</p>
+                            <p><?php echo $post['content'] ?></p>
                         </div>
                         <footer>
                             <small>♥ AREMPLACER </small>
