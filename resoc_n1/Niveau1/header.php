beb96392a6083f168c5634ba0d54eb07689bf0de
Kristine Kirakosiani <kristine.kirakossiani@gmail.com>
2023-02-16 16:56:03 +0100

    Ajouter un formulaire d’abonnement

--- resoc_n1/Niveau1/header.php
+++ resoc_n1/Niveau1/header.php
@@ -3,23 +3,23 @@ session_start();
 ?>
 
 <header>
-        <img src="resoc.jpg" alt="Logo de notre réseau social" />
-        <nav id="menu">
-            <a href="news.php">Actualités</a>
-            <a href="wall.php?user_id=<?php echo $_SESSION['connected_id']?>">Mur</a>
-            <a href="feed.php?user_id=<?php echo $_SESSION['connected_id']?>">Flux</a>
-            <a href="tags.php?tag_id=1">Mots-clés</a>
-        </nav>
-        <nav id="user">
-            <a href="#">Profil</a>
-            <ul>
-                <li><a href="settings.php?user_id=<?php echo $_SESSION['connected_id']?>">Paramètres</a></li>
-                <li><a href="followers.php?user_id=<?php echo $_SESSION['connected_id']?>">Mes suiveurs</a></li>
-                <li><a href="subscriptions.php?user_id=<?php echo $_SESSION['connected_id']?>">Mes abonnements</a></li>
-                <li><a href="registration.php?user_id=<?php echo $_SESSION['connected_id']?>">Inscription</a></li>
-                <li><a href="login.php?user_id=<?php echo $_SESSION['connected_id']?>">Connexion</a></li>
-                <li><a href="ursurpedpost.php?user_id=<?php echo $_SESSION['connected_id']?>">Apropriation de posts</a></li>
-            </ul>
+    <img src="resoc.jpg" alt="Logo de notre réseau social" />
+    <nav id="menu">
+        <a href="news.php">Actualités</a>
+        <a href="wall.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Mur</a>
+        <a href="feed.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Flux</a>
+        <a href="tags.php?tag_id=1">Mots-clés</a>
+    </nav>
+    <nav id="user">
+        <a href="#">Profil</a>
+        <ul>
+            <li><a href="settings.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Paramètres</a></li>
+            <li><a href="followers.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Mes suiveurs</a></li>
+            <li><a href="subscriptions.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Mes abonnements</a></li>
+            <li><a href="registration.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Inscription</a></li>
+            <li><a href="login.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Connexion</a></li>
+            <li><a href="usurpedpost.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Apropriation de posts</a></li>
+        </ul>
 
-        </nav>
-    </header>
+    </nav>
+</header>
--- resoc_n1/Niveau1/wall.php
+++ resoc_n1/Niveau1/wall.php
@@ -9,7 +9,7 @@
 </head>
 
 <body>
-<?php include './header.php'; ?>
+    <?php include './header.php'; ?>
     <div id="wrapper">
         <?php
         /**
@@ -33,18 +33,99 @@
             /**
              * Etape 3: récupérer le nom de l'utilisateur
              */
+
             $laQuestionEnSql = "SELECT * FROM users WHERE id= '$userId' ";
             $lesInformations = $mysqli->query($laQuestionEnSql);
             $user = $lesInformations->fetch_assoc();
+
             //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par l'alias et effacer la ligne ci-dessous
-            //echo "<pre>" . print_r($user, 1) . "</pre>";
+            echo "<pre>" . print_r($user, 1) . "</pre>";
             ?>
+
             <img src="user.jpg" alt="Portrait de l'utilisatrice" />
             <section>
                 <h3>Présentation</h3>
                 <p>Sur cette page vous trouverez tous les message de l'utilisatrice : <?php echo $user['alias'] ?>
+
                     (n° <?php echo $userId ?>)
                 </p>
+                <main>
+                    <article>
+                        <h2>Poster un message</h2>
+                        <?php
+                        /**
+                         * BD
+                         */
+                        $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+                        /**
+                         * Récupération de la liste des auteurs
+                         */
+                        $listAuteurs = [];
+                        $laQuestionEnSql = "SELECT * FROM users";
+                        $lesInformations = $mysqli->query($laQuestionEnSql);
+                        $session_actuelle = $_SESSION['connected_id'];
+                        while ($user = $lesInformations->fetch_assoc()) {
+                            if ($session_actuelle == $user['id']) {
+                                $listAuteurs[$user['id']] = $user['alias'];
+                            }
+                        }
+
+
+                        /**
+                         * TRAITEMENT DU FORMULAIRE
+                         */
+                        // Etape 1 : vérifier si on est en train d'afficher ou de traiter le formulaire
+                        // si on recoit un champs email rempli il y a une chance que ce soit un traitement
+                        $enCoursDeTraitement = isset($_POST['auteur']);
+
+                        if ($enCoursDeTraitement) {
+                            // on ne fait ce qui suit que si un formulaire a été soumis.
+                            // Etape 2: récupérer ce qu'il y a dans le formulaire @todo: c'est là que votre travaille se situe
+                            // observez le résultat de cette ligne de débug (vous l'effacerez ensuite)
+                            echo "<pre>" . print_r($_POST, 1) . "</pre>";
+                            // et complétez le code ci dessous en remplaçant les ???
+                            $authorId = $_POST['auteur'];
+                            $postContent = $_POST['message'];
+
+
+                            //Etape 3 : Petite sécurité
+                            // pour éviter les injection sql : https://www.w3schools.com/sql/sql_injection.asp
+                            $authorId = intval($mysqli->real_escape_string($authorId));
+                            $postContent = $mysqli->real_escape_string($postContent);
+                            //Etape 4 : construction de la requete
+                            $lInstructionSql = "INSERT INTO posts "
+                                . "(id, user_id, content, created, parent_id) "
+                                . "VALUES (NULL, "
+                                . $authorId . ", "
+                                . "'" . $postContent . "', "
+                                . "NOW(), "
+                                . "NULL);";
+                            echo $lInstructionSql;
+                            // Etape 5 : execution
+                            $ok = $mysqli->query($lInstructionSql);
+                            if (!$ok) {
+                                echo "Impossible d'ajouter le message: " . $mysqli->error;
+                            } else {
+                                echo "Message posté en tant que :" . $listAuteurs[$authorId];
+                            }
+                        }
+                        ?>
+                        <form action="" method="post">
+                            <input type='hidden' name='???' value='achanger'>
+                            <dl>
+                                <dt><label for='auteur'>Auteur</label></dt>
+                                <dd><select name='auteur'>
+                                        <?php
+                                        foreach ($listAuteurs as $id => $alias)
+                                            echo "<option value='$id'>$alias</option>";
+                                        ?>
+                                    </select></dd>
+                                <dt><label for='message'>Message</label></dt>
+                                <dd><textarea name='message'></textarea></dd>
+                            </dl>
+                            <input type='submit'>
+                        </form>
+                    </article>
             </section>
         </aside>
         <main>
